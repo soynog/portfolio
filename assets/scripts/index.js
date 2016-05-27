e0fef77291cae81389813c7a1fd8b6a44403306c
soynog <nicolas.o.garcia@gmail.com>
2016-05-27 17:31:10 -0400

    Factor Scroll Function into a separate script file

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,42 +1,12 @@
 'use strict';
 
 const portfolio = require('./app-data').portfolio;
+const scrollAnimate = require('./scroll');
 
 $(document).ready( function() {
   // Render Portfolio cards
   let portfolioCards = require('./templates/portfolio-cards.handlebars');
   $('.portfolio-cards').append(portfolioCards({portfolio}));
-
-  // Page Scroll Animation
-  (function (jQuery) {
-    jQuery.mark = {
-      jump: function (options) {
-        var defaults = {
-          selector: 'li.scroll-on-page-link'
-        };
-        if (typeof options === 'string') {
-          defaults.selector = options;
-        }
-
-        options = jQuery.extend(defaults, options);
-        return jQuery(options.selector).click(function (e) {
-          var jumpobj = jQuery(this);
-          console.log(jumpobj);
-          var target = jumpobj.attr('href');
-          console.log(target);
-          var thespeed = 600;
-          console.log($(target));
-          let menuWidth = $('li.scroll-on-page-link').height();
-          console.log(menuWidth);
-          var offset = jQuery(target).offset().top - menuWidth;
-          jQuery('html,body').animate({
-            scrollTop: offset
-          }, thespeed, 'swing');
-          e.preventDefault();
-        });
-      }
-    };
-  })(jQuery);
 
   // Navbar Menu
   $(".menu-expand").on('click', function(e) {
@@ -45,7 +15,8 @@ $(document).ready( function() {
     e.preventDefault();
   });
 
-
+  // Page Scroll Animation
+  scrollAnimate(jQuery);
   jQuery(function(){
     jQuery.mark.jump();
   });
--- assets/scripts/scroll.js
+++ assets/scripts/scroll.js
@@ -1,10 +1,10 @@
 'use strict';
 
-(function (jQuery) {
+const scrollAnimate = function (jQuery) {
   jQuery.mark = {
     jump: function (options) {
       var defaults = {
-        selector: 'a.scroll-on-page-link'
+        selector: 'li.scroll-on-page-link'
       };
       if (typeof options === 'string') {
         defaults.selector = options;
@@ -13,9 +13,14 @@
       options = jQuery.extend(defaults, options);
       return jQuery(options.selector).click(function (e) {
         var jumpobj = jQuery(this);
+        console.log(jumpobj);
         var target = jumpobj.attr('href');
-        var thespeed = 1000;
-        var offset = jQuery(target).offset().top;
+        console.log(target);
+        var thespeed = 600;
+        console.log($(target));
+        let menuWidth = $('li.scroll-on-page-link').height();
+        console.log(menuWidth);
+        var offset = jQuery(target).offset().top - menuWidth;
         jQuery('html,body').animate({
           scrollTop: offset
         }, thespeed, 'swing');
@@ -23,9 +28,6 @@
       });
     }
   };
-})(jQuery);
+};
 
-
-jQuery(function(){
-  jQuery.mark.jump();
-});
+module.exports = scrollAnimate;
