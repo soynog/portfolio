4926e6d4d0c69f13d032aeae1859595eafd1f12a
soynog <nicolas.o.garcia@gmail.com>
2016-05-30 13:55:44 -0400

    Add Portfolio Card Content

--- assets/scripts/app-data.js
+++ assets/scripts/app-data.js
@@ -2,29 +2,33 @@
 
 const portfolio = [
   {
-    title: "Campify",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, officiis sunt neque facilis culpa molestiae necessitatibus delectus veniam provident."
-  },
-  {
-    title: "RezTailor",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, officiis sunt neque facilis culpa molestiae necessitatibus delectus veniam provident."
+    title: "OTO",
+    desc: "OTO (Outdoor Trip Organizer) makes organizing a camping trip fun and easy! Built on Node.js, MongoDB, Ember.js, Bourbon. UX design by Norman Wozniak.",
   },
   {
     title: "SoftSurv",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, officiis sunt neque facilis culpa molestiae necessitatibus delectus veniam provident."
+    desc: "Create single-scoop surveys for your friends! Each survey may be shared via a unique public URL. Built on Node.js, MongoDB, Express.js, and Bootstrap.",
+  },
+  {
+    title: "RezTailor",
+    desc: "Tailor documents for specific needs by selecting modular sections from a master version. Rails/PostgreSQL back-end uses polymorphism to allow arbitrarily nested hierarchical document sections."
   },
   {
     title: "Tic-Tac-Toe",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, officiis sunt neque facilis culpa molestiae necessitatibus delectus veniam provident."
+    desc: "This simple game app requires user authentication and allows remote play between two players. Leverages a back-end resource watcher to update the game state in real time in response to player activity."
+  },
+  {
+    title: "Stache.it",
+    desc: "Pin a mustache to your favorite selfie! Stache.it uses Microsoft’s facial recognition API and a custom placement algorithm to automatically draw mustaches onto uploaded images. Created for a 24-hour hackathon challenge."
+  },
+  {
+    title: "Cryptography",
+    desc: "I research and presented this brief overview of cryptography, past and present, for a student conference at General Assembly."
   },
   {
     title: "This Very Website",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, officiis sunt neque facilis culpa molestiae necessitatibus delectus veniam provident."
+    desc: "Custom-built front-end using the lightweight and flexible Bourbon framework. Uses no templates or pre-made themes."
   },
-  {
-    title: $(document).width(),
-    desc: ""
-  }
 ];
 
 module.exports = {
